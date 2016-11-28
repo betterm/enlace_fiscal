718480a34efd1c2a5963b49545f62252ae6ab9aa
mikhail_kibalnyi <mishakib91@icloud.com>
2016-11-29 00:15:26 +0300

    add timezone for fechaEmission

--- lib/enlace/fiscal/invoice.rb
+++ lib/enlace/fiscal/invoice.rb
@@ -70,7 +70,7 @@ module Enlace
           'versionEF' => EF.version,
           'serie' => serie,
           'folioInterno' => folio,
-          'fechaEmision' => format_date(DateTime.now),
+          'fechaEmision' => format_date(DateTime.now.in_time_zone("America/Mexico_City")),
           'subTotal' => format_decimal(subtotal),
           'descuentos' => format_decimal(0),
           'total' => format_decimal(total),
